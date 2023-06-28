74930ea8b2ac1aad23eaa4955abbeb1fb307c985
Jacob-Lindsay <circuit.dance123@gmail.com>
2023-06-28 13:57:31 -0400

    adding hamburger menu for mobile

--- script/script.js
+++ script/script.js
@@ -10,3 +10,11 @@ function displaySite() {
     document.getElementById("site-loader").style.display = "none";
     document.getElementById("main").style.display = "block";
 }
+
+function openNav() {
+    document.getElementById("nav").style.width = "100%";
+}
+
+function closeNav() {
+    document.getElementById("nav").style.width = "0%";
+}
